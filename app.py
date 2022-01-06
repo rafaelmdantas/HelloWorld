bfc3afa8bc79655a204993195abf6a30eccd985f
rafaelmdantas <rafaelmdantas@gmail.com>
2022-01-06 18:37:37 -0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,29 @@
+# Código de Pytho é executado linha por linha
+# Definindo Strings (série de caracteres)
+print("Rafael Dantas")
+print('o----')
+print(' ||||')
+# Adicionando um operador
+# Isso é uma expressão, elas produzem valor
+print('*' * 10)
+
+# Variáveis. Elas armazenam, temporariamente, um dado.
+# Values: Integer != float != Boolean
+price = 10
+rating = 4.9
+name = 'Rafael'
+is_published = False
+print(price)
+# Exeercício: definir 3 variáveis para um paciente
+full_name = 'John Smith'
+age = 20
+is_new = True
+
+# Getting Input
+name = input('What is your name? ')
+print('Hi, ' + name)
+# Exercício: extender o programa e perguntar duas perguntas.
+# Nome da pessoa e cor favorita. Então printe uma mensagem "Rafa gosta de azul
+name = input('What is your full name? ')
+favorite_color = input('What is your favorite color? ')
+print(name + ' likes ' + favorite_color)
