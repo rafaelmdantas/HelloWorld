a9daa0cacd844c0e00279afaa0acfa03518de8a2
rafaelmdantas <rafaelmdantas@gmail.com>
2022-01-07 02:09:46 -0300

    Python's beginners - formated str

---
+++ PyCharm2.py
@@ -0,0 +1,18 @@
+# Formated Strings
+# Essa estratégia não é ideal
+first = 'John'
+last = 'Smith'
+message = first + ' [' + last + '] is a coder'
+print(message)
+# Formated Strings ajudam a vizualização
+msg = f'{first} [{last}] is a coder'
+print (msg)
+
+# String Methods
+course = 'Python for Beginners'
+print(len(course))
+print(course.upper())
+print(course.lower())
+print(course)
+
+
