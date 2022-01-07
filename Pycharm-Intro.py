4f39be0b4b838d6a881c9b197a172fb62b76433f
rafaelmdantas <rafaelmdantas@gmail.com>
2022-01-07 01:15:42 -0300

    Python's beginners - str

--- Pycharm-Intro.py
+++ Pycharm-Intro.py
@@ -1,4 +1,4 @@
-# Código de Pytho é executado linha por linha
+# Código de Python é executado linha por linha
 # Definindo Strings (série de caracteres)
 print("Rafael Dantas")
 print('o----')
@@ -27,3 +27,28 @@ print('Hi, ' + name)
 name = input('What is your full name? ')
 favorite_color = input('What is your favorite color? ')
 print(name + ' likes ' + favorite_color)
+
+# Type Conversion
+birth_year = input('Birth year: ')
+print(type(birth_year))
+age = 2022 - int(birth_year)
+print(type(age))
+print(age)
+# Exercicio: pergunte o peso em libras e converta para quilos no terminal
+weight_pounds = input('Weight (lbs): ')
+weight_kilos = int(weight_pounds)/2.2046
+#Quero a resposta arredondada e em kg
+print(str(int(weight_kilos)),'Kg')
+
+# Strings
+course = "Python's Course for Beginners"
+message = '''
+Hi, IBM
+
+Here is our first contact 
+
+hope to be there soon,
+Rafael.
+'''
+print (course)
+print (message[5:8])
